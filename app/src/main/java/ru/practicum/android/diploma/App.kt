d9267deb96dc492dc1f11b41ab1e48559f8149ee
Roman1233219 <opzadacha@yandex.ru>
2026-06-13 19:04:05 +0300

    АПП добавлен.- манифест обновлен для работы с библиотеками и сетью.заглушка для модулей создана

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -1,7 +1,16 @@
 package ru.practicum.android.diploma
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
-    // Пока пусто
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(appModule) // <-- Временная заглушка
+        }
+    }
 }

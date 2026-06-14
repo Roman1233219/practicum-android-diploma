b03b12e48982224f7bc390baef10253bb464e894
bezFantazee <davydovaksenia529@gmail.com>
2026-06-14 18:58:49 +0300

    Добавила BottomBar

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/FavouritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/FavouritesFragment.kt
@@ -1,7 +1,8 @@
 package ru.practicum.android.diploma.ui.fragments
 
 import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
 
-class FavouritesFragment: Fragment() {
+class FavouritesFragment: Fragment(R.layout.fragment_favourites) {
 
 }

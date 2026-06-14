ee135fdff5337ab071990531003b5880755b9c87
Roman1233219 <opzadacha@yandex.ru>
2026-06-14 10:19:15 +0300

    переделано подключение библиотек

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.12.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
     id("convention.detekt")
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 }

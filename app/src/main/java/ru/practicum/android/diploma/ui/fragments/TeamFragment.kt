962db2876d4caaee07595ad98f20f61bee12fbb3
bezFantazee <davydovaksenia529@gmail.com>
2026-06-14 18:58:49 +0300

    Добавила BottomBar

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/TeamFragment.kt
@@ -1,7 +1,8 @@
 package ru.practicum.android.diploma.ui.fragments
 
 import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
 
-class TeamFragment : Fragment() {
+class TeamFragment : Fragment(R.layout.fragment_team) {
 
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancySearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancySearchFragment.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.ui.fragments
 
 import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
 
-class VacancySearchFragment : Fragment() {
+class VacancySearchFragment : Fragment(R.layout.fragment_vacancy_search) {
 }

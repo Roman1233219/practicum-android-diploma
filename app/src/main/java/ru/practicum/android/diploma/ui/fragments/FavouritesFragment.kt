2875c9631edd0fe35047038e76b892d6ba095dc0
bezFantazee <davydovaksenia529@gmail.com>
2026-06-14 20:09:39 +0300

    Поправила стиль кода

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/FavouritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/FavouritesFragment.kt
@@ -3,6 +3,4 @@ package ru.practicum.android.diploma.ui.fragments
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
 
-class FavouritesFragment: Fragment(R.layout.fragment_favourites) {
-
-}
+class FavouritesFragment : Fragment(R.layout.fragment_favourites)

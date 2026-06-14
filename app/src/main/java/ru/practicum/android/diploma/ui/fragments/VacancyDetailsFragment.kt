6bc5134738cc8ecfd8a40ed1341ed88213c0bf4e
bezFantazee <davydovaksenia529@gmail.com>
2026-06-14 21:06:05 +0300

    Добавила заглушки для экранов внутреней навигации

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancyDetailsFragment.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.ui.fragments
+
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+
+class VacancyDetailsFragment : Fragment(R.layout.fragment_vacancy_details)
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/filter/RegionSelectionFragment.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.ui.fragments.filter
+
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+
+class RegionSelectionFragment : Fragment(R.layout.fragment_region_selection)

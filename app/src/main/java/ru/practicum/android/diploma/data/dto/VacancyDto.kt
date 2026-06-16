7b22d0e1959ed4034801660a07f9cb2f6a48160e
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-06-16 23:33:35 +0700

    removed unused import

--- app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDto.kt
@@ -1,5 +1,3 @@
 package ru.practicum.android.diploma.data.dto
 
-import kotlin.uuid.Uuid
-
 data class VacancyDto(val id: String, val name: String, val company: String, val city: String, val salary: SalaryDto, val logo: String)

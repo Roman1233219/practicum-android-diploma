4fb511130b6c9e59c223b54d74717bc2be43bedd
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-06-16 20:32:13 +0300

    implemented test api call

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,20 +1,27 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.data.NetworkClient
+import ru.practicum.android.diploma.data.dto.VacanciesRequest
+import ru.practicum.android.diploma.data.dto.VacanciesResponse
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
 import ru.practicum.android.diploma.presentation.SomeViewModel
 
 class RootActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRootBinding
     private val someViewModel: SomeViewModel by viewModel()
-
+    private val networkClient: NetworkClient by inject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRootBinding.inflate(layoutInflater)
@@ -33,6 +40,18 @@ class RootActivity : AppCompatActivity() {
 
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        // Тестовый вызов API HH из Activity
+        lifecycleScope.launch {
+            val testData = networkClient.searchVacancies(VacanciesRequest())
+            Log.d("vacancies", "HTTP result: ${testData.resultCode}")
+            if(testData is VacanciesResponse) {
+                Log.d("vacancies", "Page: ${testData.page}")
+                Log.d("vacancies", "Pages: ${testData.pages}")
+                Log.d("vacancies", "Found: ${testData.found}")
+                Log.d("vacancies", "First vacancy name: ${testData.items.firstOrNull()?.name}")
+            }
+        }
     }
 
     companion object {

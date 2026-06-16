986458157856410fd28eaa9734034daa3eeba1e6
Roman1233219 <opzadacha@yandex.ru>
2026-06-16 19:21:15 +0300

    правка пути HTTP- ропущен слеш в конце - в модуле убран слеш

--- app/src/main/java/ru/practicum/android/diploma/data/network/PracticumApiService.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/PracticumApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 import ru.practicum.android.diploma.data.dto.FilterAreaResponse
 
 interface PracticumApiService {
-    @GET("/areas")
+    @GET("areas")
     suspend fun getAreas(): FilterAreaResponse
 }

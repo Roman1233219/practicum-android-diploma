e6be1fe2b3f7ccb6582a44a0b1e88d01f0d7ac05
Khazz Enberg <khazzenberg@vk.com>
2026-06-16 15:32:05 +0300

    Разрешил конфликт в RootActivity

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,17 +1,15 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.presentation.SomeViewModel
-import ru.practicum.android.diploma.BuildConfig
-import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
+import ru.practicum.android.diploma.presentation.SomeViewModel
 
 class RootActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRootBinding
@@ -23,11 +21,10 @@ class RootActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Для проверки DI
-        val textView = findViewById<TextView>(R.id.buildConfigReadExampleTextView)
         someViewModel.data.observe(
             this,
             Observer { newData ->
-                textView.text = newData
+                Toast.makeText(this, "$newData", Toast.LENGTH_SHORT).show()
             }
         )
 

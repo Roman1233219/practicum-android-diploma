337ab36218a209c535189433814652bca42c769a
Roman1233219 <opzadacha@yandex.ru>
2026-06-18 18:07:28 +0300

    слияние с веткой 7

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,21 +1,20 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
-import android.util.Log
+
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
+
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.coroutines.launch
+
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.data.NetworkClient
-import ru.practicum.android.diploma.data.dto.VacanciesRequest
-import ru.practicum.android.diploma.data.dto.VacanciesResponse
+
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
 import ru.practicum.android.diploma.presentation.SomeViewModel
 
@@ -42,17 +41,6 @@ class RootActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
 
-        // Тестовый вызов API HH из Activity
-        lifecycleScope.launch {
-            val testData = networkClient.searchVacancies(VacanciesRequest())
-            Log.d("vacancies", "HTTP result: ${testData.resultCode}")
-            if (testData is VacanciesResponse) {
-                Log.d("vacancies", "Page: ${testData.page}")
-                Log.d("vacancies", "Pages: ${testData.pages}")
-                Log.d("vacancies", "Found: ${testData.found}")
-                Log.d("vacancies", "First vacancy name: ${testData.items.firstOrNull()?.name}")
-            }
-        }
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {

a1a1da8969aac718b7d0a48c89c664dd40ca3bd2
Roman1233219 <opzadacha@yandex.ru>
2026-06-19 20:43:43 +0300

    главный экран сделан до конца включая синий курсор

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancySearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancySearchFragment.kt
@@ -1,6 +1,8 @@
 package ru.practicum.android.diploma.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,7 +15,7 @@ class VacancySearchFragment : Fragment() {
     private var _binding: FragmentVacancySearchBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentVacancySearchBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -24,6 +26,24 @@ class VacancySearchFragment : Fragment() {
         binding.filterButton.setOnClickListener {
             findNavController().navigate(R.id.action_vacancySearchFragment_to_filtersFragment)
         }
+
+        binding.searchIcon.setOnClickListener {
+            binding.searchEditText.setText("")
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    binding.searchIcon.setImageResource(R.drawable.ic_search_24)
+                } else {
+                    binding.searchIcon.setImageResource(R.drawable.ic_close_24)
+                }
+            }
+            override fun afterTextChanged(s: Editable?) = Unit
+        }
+
+        binding.searchEditText.addTextChangedListener(textWatcher)
     }
 
     override fun onDestroyView() {

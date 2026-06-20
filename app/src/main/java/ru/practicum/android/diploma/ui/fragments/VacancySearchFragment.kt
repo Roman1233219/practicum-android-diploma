dfb7ba4d2ae97b810294f5fce10c399d7dfb23ad
Roman1233219 <opzadacha@yandex.ru>
2026-06-20 11:01:18 +0300

    главный экран сделан до конца включая синий курсор

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancySearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancySearchFragment.kt
@@ -6,6 +6,7 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
@@ -23,12 +24,17 @@ class VacancySearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupInitialState()
+        setupNoInternetState()
+        setupNoFoundState()
+
         binding.filterButton.setOnClickListener {
             findNavController().navigate(R.id.action_vacancySearchFragment_to_filtersFragment)
         }
 
         binding.searchIcon.setOnClickListener {
             binding.searchEditText.setText("")
+            showInitialState()
         }
 
         val textWatcher = object : TextWatcher {
@@ -36,6 +42,7 @@ class VacancySearchFragment : Fragment() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 if (s.isNullOrEmpty()) {
                     binding.searchIcon.setImageResource(R.drawable.ic_search_24)
+                    showInitialState()
                 } else {
                     binding.searchIcon.setImageResource(R.drawable.ic_close_24)
                 }
@@ -44,6 +51,45 @@ class VacancySearchFragment : Fragment() {
         }
 
         binding.searchEditText.addTextChangedListener(textWatcher)
+    }
+
+    private fun setupInitialState() {
+        binding.layoutInitial.ivPlaceholderPicture.setImageResource(R.drawable.main_screen)
+        binding.layoutInitial.tvPlaceholderText.isVisible = false
+    }
+
+    private fun setupNoInternetState() {
+        binding.layoutNoInternet.ivPlaceholderPicture.setImageResource(R.drawable.placeholder_no_internet)
+        binding.layoutNoInternet.tvPlaceholderText.text = getString(R.string.no_internet)
+        binding.layoutNoInternet.tvPlaceholderText.isVisible = true
+    }
+
+    private fun setupNoFoundState() {
+        binding.layoutNoFound.ivPlaceholderPicture.setImageResource(R.drawable.placeholder_no_found)
+        binding.layoutNoFound.tvPlaceholderText.text = getString(R.string.error_fetching_vacancies)
+        binding.layoutNoFound.tvPlaceholderText.isVisible = true
+    }
+
+    private fun showInitialState() {
+        binding.layoutInitial.root.isVisible = true
+        binding.layoutNoInternet.root.isVisible = false
+        binding.layoutNoFound.root.isVisible = false
+        binding.tvResultInfo.isVisible = false
+    }
+
+    private fun showNoInternetState() {
+        binding.layoutInitial.root.isVisible = false
+        binding.layoutNoInternet.root.isVisible = true
+        binding.layoutNoFound.root.isVisible = false
+        binding.tvResultInfo.isVisible = false
+    }
+
+    private fun showEmptyResultState() {
+        binding.layoutInitial.root.isVisible = false
+        binding.layoutNoInternet.root.isVisible = false
+        binding.layoutNoFound.root.isVisible = true
+        binding.tvResultInfo.isVisible = true
+        binding.tvResultInfo.text = getString(R.string.no_vacancies)
     }
 
     override fun onDestroyView() {

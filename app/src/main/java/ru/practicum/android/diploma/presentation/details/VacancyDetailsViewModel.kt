625b883106d24ab5cd0d297086de3edee6d66333
bezFantazee <davydovaksenia529@gmail.com>
2026-06-22 11:56:20 +0300

    Оюавила обработку одноразовых событий во viewModel

--- app/src/main/java/ru/practicum/android/diploma/presentation/details/VacancyDetailsViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/details/VacancyDetailsViewModel.kt
@@ -2,8 +2,10 @@ package ru.practicum.android.diploma.presentation.details
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.domain.api.VacanciesInteractor
@@ -16,6 +18,9 @@ class VacancyDetailsViewModel(
 
     private val _state = MutableStateFlow<VacancyDetailsState>(VacancyDetailsState.Loading)
     val state: StateFlow<VacancyDetailsState> = _state.asStateFlow()
+
+    private val _events = MutableSharedFlow<VacancyDetailsEvent>()
+    val events = _events.asSharedFlow()
 
     init {
         loadVacancyDetails()
@@ -36,6 +41,31 @@ class VacancyDetailsViewModel(
                     }
                 }
             }
+        }
+    }
+
+    // обработка одноразовых событий
+    fun onPhoneClick(phone: String) {
+        viewModelScope.launch {
+            _events.emit(VacancyDetailsEvent.OpenPhone(phone))
+        }
+    }
+
+    fun onEmailClick(email: String) {
+        viewModelScope.launch {
+            _events.emit(VacancyDetailsEvent.OpenEmail(email))
+        }
+    }
+
+    fun onUrlClick(url: String) {
+        viewModelScope.launch {
+            _events.emit(VacancyDetailsEvent.OpenUrl(url))
+        }
+    }
+
+    fun shareText(text: String) {
+        viewModelScope.launch {
+            _events.emit(VacancyDetailsEvent.Share(text))
         }
     }
 }

3c01c38883f2815c901e16f398bb9736c21f6ee5
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-06-23 11:25:29 +0300

    implemented FavoritesRepository

---
+++ app/src/main/java/ru/practicum/android/diploma/data/repository/FavoritesRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package ru.practicum.android.diploma.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.data.converters.toDatabaseEntity
+import ru.practicum.android.diploma.data.converters.toModel
+import ru.practicum.android.diploma.data.db.VacancyDao
+import ru.practicum.android.diploma.domain.api.FavoritesRepository
+import ru.practicum.android.diploma.domain.models.Vacancy
+
+class FavoritesRepositoryImpl(private val vacancyDao: VacancyDao) : FavoritesRepository {
+    override fun getFavoriteVacancies(): Flow<List<Vacancy>> = flow {
+        emit(vacancyDao.getFavoriteVacancies().map { it.toModel() })
+    }
+
+    override suspend fun getVacancy(vacancyId: String): Vacancy? {
+        return vacancyDao.getVacancyById(vacancyId)?.toModel()
+    }
+
+    override suspend fun addVacancy(vacancy: Vacancy) {
+        vacancyDao.insertVacancy(vacancy.toDatabaseEntity())
+    }
+
+    override suspend fun removeVacancy(vacancyId: String) {
+        vacancyDao.deleteVacancy(vacancyId)
+    }
+}

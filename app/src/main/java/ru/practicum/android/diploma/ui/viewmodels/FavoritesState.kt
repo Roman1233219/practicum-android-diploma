7ae9673bff8ff18bd37e54b24b4b81aa9a501b3f
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-06-23 12:21:22 +0300

    removed error message

--- app/src/main/java/ru/practicum/android/diploma/ui/viewmodels/FavoritesState.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/viewmodels/FavoritesState.kt
@@ -5,6 +5,6 @@ import ru.practicum.android.diploma.domain.models.Vacancy
 sealed interface FavoritesState {
     object Loading : FavoritesState
     data class Content(val vacancies: List<Vacancy>) : FavoritesState
-    data class Error(val message: String) : FavoritesState
+    object Error : FavoritesState
     object Empty: FavoritesState
 }

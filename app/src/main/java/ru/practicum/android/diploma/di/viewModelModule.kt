a961a2d97784008a0638ab4fa6b9b4af5674bda1
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-06-23 15:41:30 +0300

    fixed DI mismatch

--- app/src/main/java/ru/practicum/android/diploma/di/viewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/viewModelModule.kt
@@ -12,6 +12,6 @@ val viewModelModule = module {
     viewModel { FavouritesViewModel(get()) }
 
     viewModel { (vacancyId: String) ->
-        VacancyDetailsViewModel(vacancyId, get())
+        VacancyDetailsViewModel(vacancyId, get(),get())
     }
 }

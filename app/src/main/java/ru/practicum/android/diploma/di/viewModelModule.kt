e91257440035d6bccfefbcc4fd0771de4529eeb8
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-06-23 12:23:13 +0300

    register FavouritesViewModel in DI

--- app/src/main/java/ru/practicum/android/diploma/di/viewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/viewModelModule.kt
@@ -3,10 +3,14 @@ package ru.practicum.android.diploma.di
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.details.VacancyDetailsViewModel
+import ru.practicum.android.diploma.ui.viewmodels.FavouritesViewModel
 import ru.practicum.android.diploma.ui.viewmodels.SearchViewModel
 
 val viewModelModule = module {
     viewModel { SearchViewModel(get()) }
+
+    viewModel { FavouritesViewModel(get()) }
+
     viewModel { (vacancyId: String) ->
         VacancyDetailsViewModel(vacancyId, get())
     }

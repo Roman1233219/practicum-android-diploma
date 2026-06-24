36d4a2e3620cedbd36d1500f9943715d03a34cd1
Roman1233219 <opzadacha@yandex.ru>
2026-06-24 18:43:46 +0300

    мелкие правки для чистой архитектуры часть 2

--- app/src/main/java/ru/practicum/android/diploma/presentation/viewmodels/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/viewmodels/SearchViewModel.kt
@@ -1,6 +1,5 @@
-package ru.practicum.android.diploma.ui.viewmodels
+package ru.practicum.android.diploma.presentation.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope

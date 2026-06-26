0ec686e36149d699aaba0d4fa43a98203b2b1f72
Roman1233219 <opzadacha@yandex.ru>
2026-06-26 09:46:24 +0300

    создан итерактр и прокинут в репозиторий2

--- app/src/main/java/ru/practicum/android/diploma/di/viewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/viewModelModule.kt
@@ -4,12 +4,15 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.details.VacancyDetailsViewModel
 import ru.practicum.android.diploma.presentation.viewmodels.FavouritesViewModel
+import ru.practicum.android.diploma.presentation.viewmodels.FiltersViewModel
 import ru.practicum.android.diploma.presentation.viewmodels.SearchViewModel
 
 val viewModelModule = module {
     viewModel { SearchViewModel(get()) }
 
     viewModel { FavouritesViewModel(get()) }
+
+    viewModel { FiltersViewModel(get()) }
 
     viewModel { (vacancyId: String) ->
         VacancyDetailsViewModel(vacancyId, get(),get())

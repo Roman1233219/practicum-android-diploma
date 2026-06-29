23a4079bfa06158bad0290a47c72e2c8fd387539
Roman1233219 <opzadacha@yandex.ru>
2026-06-29 18:58:43 +0300

    исправляем придирки от детекта

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/IndustryViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/IndustryViewHolder.kt
@@ -8,18 +8,18 @@ import ru.practicum.android.diploma.domain.models.Industry
 
 class IndustryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val radioButton: RadioButton = itemView.findViewById(R.id.name)
-    
+
     fun bind(model: Industry, isSelected: Boolean, onCheckedChangeListener: (Boolean) -> Unit) {
         radioButton.text = model.industryName
-        
+
         // Снимаем слушатель перед установкой значения, чтобы не зациклиться
         radioButton.setOnCheckedChangeListener(null)
         radioButton.isChecked = isSelected
-        
+
         radioButton.setOnCheckedChangeListener { _, isChecked ->
             onCheckedChangeListener(isChecked)
         }
-        
+
         // Клик по самому айтему тоже должен менять состояние
         itemView.setOnClickListener {
             radioButton.isChecked = !radioButton.isChecked

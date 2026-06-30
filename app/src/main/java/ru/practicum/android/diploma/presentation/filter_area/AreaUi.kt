1725ffccd97dd7ac141532e755f55d448144509f
Roman1233219 <opzadacha@yandex.ru>
2026-06-30 20:08:34 +0300

    исправлено замечание от детект по поводу шаблона

--- app/src/main/java/ru/practicum/android/diploma/presentation/filter_area/AreaUi.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter_area/AreaUi.kt
@@ -1,4 +1,4 @@
-package ru.practicum.android.diploma.presentation.`filter-area`
+package ru.practicum.android.diploma.presentation.filter_area
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
--- app/src/main/java/ru/practicum/android/diploma/presentation/filter_area/CountryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter_area/CountryViewModel.kt
@@ -1,4 +1,4 @@
-package ru.practicum.android.diploma.presentation.`filter-area`
+package ru.practicum.android.diploma.presentation.filter_area
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
